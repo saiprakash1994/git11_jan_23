f13418f8282b9bf3c70778bfb394d0ca55e07431
saiprakash talakanti <talakantisaiprakash@gmail.com>
2023-01-11 22:08:51 +0530

    git pull

--- oneapp/src/App.js
+++ oneapp/src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>praneeth First Remote app</h1>
       <h1>sai Prakash First Remote app</h1>
+      <h1>praneeth First Remote app</h1>
       <h1>sai Prakash First Remote app</h1>
-      <h1>sai Prakash First Remote app</h1>
-      <h1>sai Prakash First Remote app</h1>
-      <h1>sai Prakash First Remote app</h1>
+      <h1>praneeth First Remote app</h1>
     </div>
   );
 }
