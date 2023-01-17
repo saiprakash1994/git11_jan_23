ad2dd9392f4c592086bb0337c5fc50c8441d9a88
Nasa Dheeraj <dheeraj6082@gmail.com>
2023-01-17 13:17:34 +0530

    Created ipl Images Folder and Added ipl Teams to App.js and made necessary changes to App.css

--- oneapp/src/App.js
+++ oneapp/src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
@@ -10,6 +10,32 @@ function App() {
       <h1>sai Prakash First Remote app</h1>
       <h1>praneeth First Remote app</h1>
       <h1>brn</h1>
+
+      <div className="App">
+    <h1>IPL Teams</h1>
+    <div className="IPL">
+
+<div>
+  <img src=".\ipl Images\CSK.png"></img>
+  <h3>Chennai Super Kings</h3>
+</div>
+
+<div>
+  <img src=".\ipl Images\MI.png"></img>
+  <h3>Mumbai Indians</h3>
+</div>
+
+<div>
+  <img src=".\ipl Images\RCB.jpg"></img>
+  <h3>Royal Challengers Banglore</h3>
+</div>
+
+<div>
+  <img src=".\ipl Images\SRH.png"></img>
+  <h3>Chennai Super Kings</h3>
+</div>
+    </div>
+    </div>
     </div>
   );
 }
